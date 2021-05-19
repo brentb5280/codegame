4779326c41c02e578586e5f5395f30e0b403e720
Brent Bowles <bbowles@sileotech.com>
2021-05-18 21:31:23 -0700

    hope this works

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+// Putting my questions and answers on a seperate page
+let questions = [
+    {
+    numb: 1,
+    question: "What does Javascript stand for?",
+    answer: "A client-side scripting language",
+    options: [
+      "A really good coffee shop",
+      "A client-side scripting language",
+      "A magazine about coffee",
+      "Hypertext Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following are capabilities of functions in JavaScript?",
+    answer: "Accept parameters",
+    options: [
+      "Accept parameters",
+      "Accept parameters and Return a value",
+      "Return a value",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following best describes JavaScript?",
+    answer: "An object-oriented scripting language.",
+    options: [
+      "A low-level programming language.",
+      "A scripting language precompiled in the browser.",
+      "A compiled scripting language.",
+      "An object-oriented scripting language."
+    ]
+  },
+    {
+    numb: 4,
+    question: "In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.",
+    answer: "A wrapper",
+    options: [
+      "A form",
+      "A cursor",
+      "A link",
+      "A wrapper"
+    ]
+  },
+   
+  ];
